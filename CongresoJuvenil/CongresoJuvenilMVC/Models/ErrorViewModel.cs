6df7b4c5ff3ce4897dfe4c79753b08cdee761e6e
Raycel Graterol <craycel.0@gmail.com>
2021-07-10 06:56:24 -0400

    MVC and publish

---
+++ CongresoJuvenil/CongresoJuvenilMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CongresoJuvenilMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
